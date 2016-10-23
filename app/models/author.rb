ed0b35721f35b572ae9b96e5039b63218b917611
gerald hernandez <gerald2557@gmail.com>
2016-10-23 12:43:08 -0700

    set up tests for authors

---
+++ app/models/author.rb
@@ -0,0 +1,4 @@
+class Author < ApplicationRecord
+    validates :family_name, presence: true
+    validates :given_name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023191344) do
+ActiveRecord::Schema.define(version: 20161023193432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "given_name"
+    t.string   "family_name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "publishers", force: :cascade do |t|
     t.string   "name"
