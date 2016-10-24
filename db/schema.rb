266812d787146da0f3c7ad1b66a80c5626091c3d
gerald hernandez <gerald2557@gmail.com>
2016-10-23 23:09:32 -0700

    created book model and some tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023193432) do
+ActiveRecord::Schema.define(version: 20161024035451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,30 @@ ActiveRecord::Schema.define(version: 20161023193432) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.string   "isbn_10"
+    t.string   "isbn_13"
+    t.text     "description"
+    t.date     "released_on"
+    t.integer  "publisher_id"
+    t.integer  "author_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["author_id"], name: "index_books_on_author_id", using: :btree
+    t.index ["isbn_10"], name: "index_books_on_isbn_10", using: :btree
+    t.index ["isbn_13"], name: "index_books_on_isbn_13", using: :btree
+    t.index ["publisher_id"], name: "index_books_on_publisher_id", using: :btree
+    t.index ["title"], name: "index_books_on_title", using: :btree
+  end
+
   create_table "publishers", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "books", "authors"
+  add_foreign_key "books", "publishers"
 end
